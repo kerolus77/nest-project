49308c5fdd43b2ddb9ed7dffbaeb139aa141bea1
kerolus77 <125474671+kerolus77@users.noreply.github.com>
2026-09-11 02:20:05 +0300

    feat(users): add user DTOs and entity
    - Created CreateUserDto for user creation with name, email, and password fields.
    - Implemented UpdateUserDto extending CreateUserDto for partial updates.
    - Added UserEntity class to represent user data structure.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,29 +1,48 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto.js';
+import { UpdateUserDto } from './dtos/update-user.dto.js';
+import { UserEntity } from './user.entity.js';
+import {v4 as uuid} from 'uuid';
 @Controller('users')
 export class UsersController {
-
+private users:UserEntity[] = [];
     @Get()
     find() {
-        return 'This action returns all users';
+        return this.users;
     }
 
     @Get(':id')
-    findOne() {
-        return 'This action returns a user';
+    findOne(@Param('id') id: string) {
+        return this.users.find(user=>user.id ===id);
     }
 
     @Post()
-    create() {
-        return 'This action creates a new user';
+    create(@Body() userData: CreateUserDto) {
+        const newUser:UserEntity={
+            id: uuid(),
+            ...userData,
+            
+        }
+        this.users.push(newUser);
+        return newUser;
     }
+        
+     
 
     @Patch(':id')
-    update() {
-        return 'This action updates a user';
+    update(@Param('id') id: string,@Body() updateData: UpdateUserDto) {
+        const userIndex = this.users.findIndex(user => user.id === id);
+        if (userIndex === -1) {
+            return `User with ID: ${id} not found`;
+        }
+
+        this.users[userIndex] = { ...this.users[userIndex], ...updateData };
+        return this.users[userIndex];
     }
 
     @Delete(':id')
-    remove() {
-        return 'This action removes a user';
-    }
+    @HttpCode(HttpStatus.NO_CONTENT)
+    remove(@Param('id') id: string) {
+       this.users = this.users.filter(user => user.id !== id);
+          }
 }
